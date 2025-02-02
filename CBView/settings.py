ce046cd0ebb6cbc1b2d618b77948166400937e3a
Amirreza Abolghasemi <aboliamir398@gmail.com>
2025-02-02 20:45:22 +0330

    Update settings.py

--- CBView/settings.py
+++ CBView/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e(uw%j1ou3lbepo-5#f&)9=l1+i2l@+btd-asek@3!8&f%pg7n'
+SECRET_KEY = '<YOUR_SECRETKEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
